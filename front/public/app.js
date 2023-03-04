917081ef3e0ad5b3982c8f3aa15c375de2b34d75
Julio Cesar Camargo Generoso <00436770@icnavais.net>
2023-03-03 21:04:35 -0300

    form - input arquivos e calculo de tamanho

---
+++ front/public/app.js
@@ -0,0 +1,38 @@
+let bytesAmount = 0;
+
+const formatBytes = (bytes) => {
+  const units = ['B','KB','MB','GB','TB']
+
+  let i = 0
+
+  for(i; bytes >=1024 && i < 4; i++) {
+    bytes /= 1024
+  }
+
+  return `${bytes.toFixed(2)} ${units[i]}`
+}
+
+const updateStatus = (size) => {
+  const text = `Pending Bytes to Upload: <strong>${formatBytes(size)}</strong>`
+  document.getElementById("size").innerHTML = text
+}
+
+const showSize = () => {
+  const { files: fileElements } = document.getElementById('file')
+  if(!fileElements.length) return;
+
+  const files = Array.from(fileElements)
+  const { size } = files.reduce((prev, next) => ({size: prev.size + next.size}), { size: 0 })
+
+  bytesAmount = size
+  updateStatus(size)
+  
+}
+
+const onload = () => {
+  console.log('loaded')
+}
+
+window.onload = onload
+window.showSize = showSize
+
